b3842241aa188a23dc902c920a659922f6e6df7a
slxshhh <gian.lucca1964@gmail.com>
2025-02-18 16:26:33 -0300

    fixingEnv.js

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -4,8 +4,9 @@ import "dotenv/config";
 import axios from "axios";
 
 
-const NODE_ENV = process.env;
-const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+// const NODE_ENV = process.env;
+// const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+const URL = "https://spotifyclone-u05e.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
